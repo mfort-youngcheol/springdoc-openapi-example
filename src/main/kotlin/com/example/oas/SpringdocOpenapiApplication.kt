c4e9e0b7c828ee57bb02cf4fbc28cffeb8170781
mfort-youngcheol <yc.son@mfort.co.kr>
2022-09-07 09:17:31 +0900

    init project

---
+++ src/main/kotlin/com/example/oas/SpringdocOpenapiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.oas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringdocOpenapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringdocOpenapiApplication>(*args)
+}
